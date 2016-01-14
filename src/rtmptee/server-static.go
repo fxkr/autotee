af13525168846285f31666627414bf0dbe1c23c5
Felix Kaiser <felix.kaiser@fxkr.net>
2016-01-14 18:05:38 +0100

    Forgot to add file

---
+++ src/rtmptee/server-static.go
@@ -0,0 +1,17 @@
+package rtmptee
+
+import (
+	"github.com/deckarep/golang-set"
+)
+
+type StaticStreamList struct {
+	config *Config
+}
+
+func NewStaticStreamList(config *Config) Server {
+	return &StaticStreamList{config}
+}
+
+func (nr *StaticStreamList) GetActiveStreams() (mapset.Set, error) {
+	return nr.config.Server.Streams, nil
+}
